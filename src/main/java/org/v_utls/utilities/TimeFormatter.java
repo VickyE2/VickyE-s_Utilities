3b60f3795a048ab91c710cd7582a6818782f5c55
VickyE2 <f.b.cgamingco@gmail.com>
2024-09-10 19:03:19 +0100

    converted TimeFormatter to accept both longs and integers

--- src/main/java/org/v_utls/utilities/TimeFormatter.java
+++ src/main/java/org/v_utls/utilities/TimeFormatter.java
@@ -3,7 +3,15 @@ package org.v_utls.utilities;
 public class TimeFormatter {
 
     // Static method to format time from seconds to "min:second"
-    public static String formatTime(int totalSeconds) {
+    public static String formatTimeLong(long totalSeconds) {
+        long minutes = totalSeconds / 60;
+        long seconds = totalSeconds % 60;
+
+        // Format minutes and seconds to always show two digits
+        return String.format("%02d:%02d", minutes, seconds);
+    }
+
+    public static String formatTimeInt(int totalSeconds) {
         int minutes = totalSeconds / 60;
         int seconds = totalSeconds % 60;
 

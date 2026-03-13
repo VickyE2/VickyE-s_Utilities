33ce7a1e882d24ddc013057138059b60c6a1f45a
VickyE2 <f.b.cgamingco@gmail.com>
2026-03-13 16:05:01 +0100

    item descriptor upgrade. made it more verbose.

--- shared/src/main/java/org/vicky/utilities/Pair.java
+++ shared/src/main/java/org/vicky/utilities/Pair.java
@@ -2,6 +2,10 @@
 package org.vicky.utilities;
 
 public record Pair<T, U>(T key, U value) {
+  public static <T, U> Pair<T, U> of(T key, U value) {
+    return new Pair<>(key, value);
+  }
+
   public T getKey() {
     return key;
   }
